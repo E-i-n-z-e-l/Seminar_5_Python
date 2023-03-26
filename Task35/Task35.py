a2b14dab2a86602f736693d6f98c9f3f31a1a45e
Einzel <einzel0102@mail.ru>
2023-03-26 16:15:24 +0800

    Напишите функцию, которая принимает одно число и проверяет, является ли оно простым.

--- Task35/Task35.py
+++ Task35/Task35.py
@@ -3,8 +3,8 @@
 def is_number_simple(number):
     if number != 2 and number % 2 == 0:
         return False
-    for i in range(3, number // 2 + 1, 2):
-        if number % i == 0:
+    for i in range(3, number // 2 + 1, 2): # Начинаем с числа "3", заканчиваем на "number // 2 + 1" (введенное пользователем число деленное на 2 + 1), с шагом (2) 
+        if number % i == 0:                # для того чтобы исключить все четные числа, так как они не могут быть простыми кроме 2-ки
             return False
     return True
 
